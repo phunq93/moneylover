7cba913e1e0937d4082d78dde2d3b69f461851e1
phunq <phunq93@gmail.com>
2019-09-17 14:37:46 +0700

    add group1, group2

--- db.py
+++ db.py
@@ -5,3 +5,45 @@ db = client.moneylover
 
 test_collection = db.test
 spendings_collection = db.spendings
+group1_collection = db.group1
+group2_collection = db.group2
+
+group1 = [
+	'Ăn uống',
+	'Di chuyển',
+	'Sinh hoạt',
+	'Mua sắm',
+	'Giải trí',
+	'Chăm sóc',
+	'Học tập',
+	'Cho đi',
+	'Khác',
+]
+
+group2 = [
+	{'value': 'Mua thực phẩm', 'parent': 'Ăn uống'},
+	{'value': 'Ăn ngoài', 'parent': 'Ăn uống'},
+	{'value': 'Đổ xăng', 'parent': 'Di chuyển'},
+	{'value': 'Mua vé|Gọi xe', 'parent': 'Di chuyển'},
+	{'value': 'Tiền điện', 'parent': 'Sinh hoạt'},
+	{'value': 'Tiền mạng', 'parent': 'Sinh hoạt'},
+	{'value': 'Tiền nước', 'parent': 'Sinh hoạt'},
+	{'value': 'Phí sinh hoạt', 'parent': 'Sinh hoạt'},
+	{'value': 'Gửi xe', 'parent': 'Sinh hoạt'},
+	{'value': 'Quần áo', 'parent': 'Mua sắm'},
+	{'value': 'Mỹ phẩm', 'parent': 'Mua sắm'},
+	{'value': 'Đồ dùng hàng ngày', 'parent': 'Mua sắm'},
+	{'value': 'Xem phim', 'parent': 'Giải trí'},
+	{'value': 'Chơi game', 'parent': 'Giải trí'},
+	{'value': 'Khám bệnh|Mua thuốc', 'parent': 'Chăm sóc'},
+	{'value': 'Mua sách', 'parent': 'Học tập'},
+	{'value': 'Khóa học', 'parent': 'Học tập'},
+	{'value': 'Tiền mừng', 'parent': 'Cho đi'},
+	{'value': 'Tiền thăm hỏi', 'parent': 'Cho đi'},
+	{'value': 'Mất tiền ngu', 'parent': 'Khác'},
+]
+
+# for group in group1:
+# 	group1_collection.insert_one({'value': group})
+# for group in group2:
+# 	group2_collection.insert_one({'value': group['value'], 'parent': group['parent']})
--- resources/spending.py
+++ resources/spending.py
@@ -2,15 +2,18 @@ from bson import ObjectId
 from flask import jsonify
 from flask_restful import Resource, reqparse
 
-from db import spendings_collection
+from db import spendings_collection, group1_collection, group2_collection
 from common.utils import str2dt
 
 
 class Spendings(Resource):
 	def __init__(self):
+		group1 = group1_collection.find().distinct('value')
+		group2 = group2_collection.find().distinct('value')
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('amount', type=float, required=True)
-		self.reqparse.add_argument('group', type=str, required=True)
+		self.reqparse.add_argument('group1', choices=group1, type=str, required=True)
+		self.reqparse.add_argument('group2', choices=group2, type=str, required=True)
 		self.reqparse.add_argument('detail', type=str, required=True)
 		self.reqparse.add_argument('date', type=str2dt, required=True)
 		self.reqparse.add_argument('wallet', type=str, required=True)
@@ -23,9 +26,10 @@ class Spendings(Resource):
 		args = self.reqparse.parse_args()
 		spending = {
 			'amount': args.amount,
-			'group': args.group,
+			'group1': args.group1,
+			'group2': args.group2,
 			'detail': args.detail,
-			'date': args.date,
+			'date'  : args.date,
 			'wallet': args.wallet,
 		}
 		spendings_collection.insert_one(spending)
@@ -41,7 +45,8 @@ class Spending(Resource):
 		args = self.reqparse.parse_args()
 		spending = {
 			'amount': args.amount,
-			'group' : args.group,
+			'group1': args.group1,
+			'group' : args.group2,
 			'detail': args.detail,
 			'date'  : args.date,
 			'wallet': args.wallet,
